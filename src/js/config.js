7ba1645d80cfcd8d887bb1be641a9ee281806748
bilal-aslam <bilalaslam96@outlook.com>
2022-08-15 16:23:42 +0500

    update

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '09d2843c-409f-4628-82bc-bd66fc9fe659';
+export const MODAL_CLOSE_SEC = 2.5;
